0bac27903ff6036c555952ded6f4e60418cac39c
devasenan25 <devasenan.20cs@kct.ac.in>
2023-02-16 16:45:34 +0530

    es10-50 comparison check

--- read_pickle.py
+++ read_pickle.py
@@ -1,33 +1,36 @@
 import pickle
 from tabulate import tabulate
 
-pkl_path = 'pickles/tables/'
-pkl_file = pkl_path+'pt_mt_prob.pkl'
-with open(pkl_file, 'rb') as rf:
-    data = pickle.load(rf)
-    rf.close()
-print(tabulate(data, headers='keys', tablefmt="simple_grid"))
+pkl_path = 'pickles/'
+pkl_file = pkl_path+'kbga/amazon/single_run_az_kbga-es50.pkl'
 
-pkl_file = pkl_path+'pt_co_prob.pkl'
 with open(pkl_file, 'rb') as rf:
     data = pickle.load(rf)
     rf.close()
-print(tabulate(data, headers='keys', tablefmt="simple_grid"))
+#print(tabulate(data, headers='keys', tablefmt="simple_grid"))
+print(*data[1])
 
-pkl_file = pkl_path+'pt_mt_rate.pkl'
-with open(pkl_file, 'rb') as rf:
-    data = pickle.load(rf)
-    rf.close()
-print(tabulate(data, headers='keys', tablefmt="simple_grid"))
 
-pkl_file = pkl_path+'single_run_table.pkl'
-with open(pkl_file, 'rb') as rf:
-    data = pickle.load(rf)
-    rf.close()
-print(tabulate(data, headers='keys', tablefmt="simple_grid"))
-
-pkl_file = pkl_path+'n_run_table.pkl'
-with open(pkl_file, 'rb') as rf:
-    data = pickle.load(rf)
-    rf.close()
-print(tabulate(data, headers='keys', tablefmt="simple_grid"))
+# pkl_file = pkl_path+'pt_co_prob.pkl'
+# with open(pkl_file, 'rb') as rf:
+#     data = pickle.load(rf)
+#     rf.close()
+# print(tabulate(data, headers='keys', tablefmt="simple_grid"))
+# 
+# pkl_file = pkl_path+'pt_mt_rate.pkl'
+# with open(pkl_file, 'rb') as rf:
+#     data = pickle.load(rf)
+#     rf.close()
+# print(tabulate(data, headers='keys', tablefmt="simple_grid"))
+# 
+# pkl_file = pkl_path+'single_run_table.pkl'
+# with open(pkl_file, 'rb') as rf:
+#     data = pickle.load(rf)
+#     rf.close()
+# print(tabulate(data, headers='keys', tablefmt="simple_grid"))
+# 
+# pkl_file = pkl_path+'n_run_table.pkl'
+# with open(pkl_file, 'rb') as rf:
+#     data = pickle.load(rf)
+#     rf.close()
+# print(tabulate(data, headers='keys', tablefmt="simple_grid"))
