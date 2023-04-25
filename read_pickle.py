956c8efde07c9a2606fa13ebf565fce5c7744a5a
devasenan25 <devasenan.20cs@kct.ac.in>
2023-04-25 09:20:34 +0530

    kbga vp 30 runs az save done

--- read_pickle.py
+++ read_pickle.py
@@ -3,14 +3,13 @@ from tabulate import tabulate
 import seaborn as sns
 
 pkl_path = 'pickles/'
-pkl_file = pkl_path+'ga/single_run_az_100fc.pkl'
+pkl_file = pkl_path+'kbga/30runs/30_run_az_kbga_vp_ts.pkl'
 
 with open(pkl_file, 'rb') as rf:
     data = pickle.load(rf)
     rf.close()
 #print(tabulate(data, headers='keys', tablefmt="simple_grid"))
-print(data[1])
-sns.lineplot(x=list(range(1, 101)), y=data[1])
+print(data)
 
 
 # pkl_file = pkl_path+'pt_co_prob.pkl'
